93bb6e6cefd931894f43feefde0cdca8cc3a71c8
djeukeu <dmxchristian@gmail.com>
2024-09-06 23:38:00 +0100

    auth header fix

--- src/Components/AuthHeader/index.js
+++ src/Components/AuthHeader/index.js
@@ -3,19 +3,26 @@ import { getHeaderTitle } from '@react-navigation/elements';
 import { Icon } from '@rneui/base';
 import { useTranslation } from 'react-i18next';
 import { View, Text, TouchableOpacity, Platform, Image } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import styles from './styles';
 import Colors from '../../Constants/Colors';
 
 const AuthHeader = (props) => {
   const { t } = useTranslation();
+  const insets = useSafeAreaInsets();
   const { route, options } = props;
   const title = getHeaderTitle(options, route.name);
   const backBtn = props.navigation.canGoBack();
 
   return (
-    <View style={styles.container}>
+    <View style={{ ...styles.container, marginTop: insets.top }}>
       {backBtn && (
-        <TouchableOpacity style={styles.back}>
+        <TouchableOpacity
+          style={styles.back}
+          activeOpacity={0.63}
+          onPress={() => {
+            props.navigation.goBack();
+          }}>
           <Icon
             name={Platform.OS === 'ios' ? 'arrow-back-ios' : 'arrow-back'}
             color={Colors.secondary}
